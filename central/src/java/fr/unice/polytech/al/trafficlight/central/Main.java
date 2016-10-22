f23ae27c55f71989f4491ad41658d810527b54f8
Tom DALL'AGNOL <dallagnol.tom@etu.unice.fr>
2016-10-22 21:23:51 +0200

    Add an initial configuration for the project

---
+++ central/src/java/fr/unice/polytech/al/trafficlight/central/Main.java
@@ -0,0 +1,11 @@
+package fr.unice.polytech.al.trafficlight.central;
+
+/**
+ * Created by tom dall'agnol on 22/10/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
