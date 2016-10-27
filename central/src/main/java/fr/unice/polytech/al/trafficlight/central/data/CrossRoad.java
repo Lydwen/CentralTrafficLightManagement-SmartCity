9245288eb7df5f5d00e474aa20d2991b1c2a01b2
Tom DALL'AGNOL <dallagnol.tom@etu.unice.fr>
2016-10-27 17:36:33 +0200

    Add a resource CrossRoad representing a cross road and containing a scenario

---
+++ central/src/main/java/fr/unice/polytech/al/trafficlight/central/data/CrossRoad.java
@@ -0,0 +1,124 @@
+package fr.unice.polytech.al.trafficlight.central.data;
+
+
+import fr.unice.polytech.al.trafficlight.utils.Scenario;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Represents a CrossRoad : its name, what are the
+ * traffic lights in it, what are the different axes,
+ * the address to communicate with it and the current
+ * scenario for it
+ *
+ * Created by tom dall'agnol on 27/10/16.
+ */
+public class CrossRoad {
+    private String name;
+    private String url;
+    private Scenario scenario;
+    private Set<TrafficLight> trafficLights;
+    private Set<String> roads;
+
+    public CrossRoad(String name, String url, Scenario scenario) {
+        this.name = name;
+        this.url = url;
+        this.scenario = scenario;
+        this.trafficLights = new HashSet<>();
+        this.roads = new HashSet<>();
+    }
+    public CrossRoad(String name, String url) {
+        this.name = name;
+        this.url = url;
+        this.scenario = null;
+        this.trafficLights = new HashSet<>();
+        this.roads = new HashSet<>();
+    }
+
+    public void addTrafficLight(TrafficLight trafficLight){
+        this.trafficLights.add(trafficLight);
+    }
+
+    public void addRoad(String road){
+        this.roads.add(road);
+    }
+
+    public void removeTrafficLight(TrafficLight trafficLight){
+        this.trafficLights.remove(trafficLight);
+    }
+
+    public void removeRoad(String road){
+        this.roads.remove(road);
+    }
+
+//================ GETTERs & SETTERs ================
+
+    public Set<String> getRoads() {
+        return roads;
+    }
+
+    public void setRoads(Set<String> roads) {
+        this.roads = roads;
+    }
+
+    public Set<TrafficLight> getTrafficLights() {
+        return trafficLights;
+    }
+
+    public void setTrafficLights(Set<TrafficLight> trafficLights) {
+        this.trafficLights = trafficLights;
+    }
+
+    public Scenario getScenario() {
+        return scenario;
+    }
+
+    public void setScenario(Scenario scenario) {
+        this.scenario = scenario;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//================ HashCode & Equals ================
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CrossRoad crossRoad = (CrossRoad) o;
+
+        if (!name.equals(crossRoad.name)) return false;
+        if (!url.equals(crossRoad.url)) return false;
+        if (scenario != null ? !scenario.equals(crossRoad.scenario) : crossRoad.scenario != null) return false;
+        if (trafficLights != null ? !trafficLights.equals(crossRoad.trafficLights) : crossRoad.trafficLights != null)
+            return false;
+        return roads != null ? roads.equals(crossRoad.roads) : crossRoad.roads == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + url.hashCode();
+        result = 31 * result + (scenario != null ? scenario.hashCode() : 0);
+        result = 31 * result + (trafficLights != null ? trafficLights.hashCode() : 0);
+        result = 31 * result + (roads != null ? roads.hashCode() : 0);
+        return result;
+    }
+}
---
+++ central/src/main/java/fr/unice/polytech/al/trafficlight/central/provider/CrossRoadInformationService.java
@@ -0,0 +1,82 @@
+package fr.unice.polytech.al.trafficlight.central.provider;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import fr.unice.polytech.al.trafficlight.central.data.*;
+import fr.unice.polytech.al.trafficlight.central.data.TrafficLight;
+import fr.unice.polytech.al.trafficlight.utils.RuleGroup;
+import fr.unice.polytech.al.trafficlight.utils.Scenario;
+import fr.unice.polytech.al.trafficlight.utils.enums.TrafficLightId;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tom dall'agnol on 27/10/16.
+ */
+@Path("crossroad")
+public class CrossRoadInformationService {
+    private GsonBuilder builder = new GsonBuilder();
+
+    /**
+     * Retrieves all the existing crossroads
+     * in the db. You can have more informations
+     * about the crossroad doing a GET request
+     * on the ressource crossroad/[CrossRoad name]
+     *
+     * @return a Response containing all the crossroads names
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveCrossRoads(){
+        Gson gson = builder.create();
+
+        List<String> crossRoads = new ArrayList<>();
+        crossRoads.add("carrefour_du_casino");
+        crossRoads.add("carrefour_des_pins");
+        return Response.ok()
+                .entity(gson.toJson(crossRoads))
+                .build()
+                ;
+    }
+
+    @GET
+    @Path("/{crossRoadName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveSpecifcCrossRoad(@PathParam("crossRoadName") String crossRoadName){
+        Gson gson = builder.create();
+        CrossRoad crossRoad = new CrossRoad("carrefour_du_casino", "url");
+        crossRoad.addRoad("avenue du tapis vert");
+        crossRoad.addRoad("avenue des orangers");
+
+        TrafficLight trafficLight1 = new TrafficLight("feu de l'avenue des orangers");
+        trafficLight1.addRoad("avenue des orangers");
+        TrafficLight trafficLight2 = new TrafficLight("feu de l'avenue du tapis vert");
+        trafficLight1.addRoad("avenue du tapis vert");
+
+        crossRoad.addTrafficLight(trafficLight1);
+        crossRoad.addTrafficLight(trafficLight2);
+
+        Scenario scenar = new Scenario("basicScenario");
+        RuleGroup group1 = new RuleGroup("group1", 20);
+        RuleGroup group2 = new RuleGroup("group2", 40);
+        TrafficLightId id1 = new TrafficLightId("feu de l'avenue des orangers");
+        TrafficLightId id2 = new TrafficLightId("feu de l'avenue du tapis vert");
+        group1.addTrafficLight(id1);
+        group1.addTrafficLight(id2);
+        group2.addTrafficLight(id2);
+
+        scenar.addRuleGroupList(0, group1);
+        scenar.addRuleGroupList(1, group2);
+        scenar.setTransitionTime(5);
+
+        crossRoad.setScenario(scenar);
+        return Response.ok().entity(gson.toJson(crossRoad)).build();
+    }
+}
