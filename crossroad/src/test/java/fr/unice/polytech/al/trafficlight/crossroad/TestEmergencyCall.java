099fedc5fea14efd755db29f72930a645d5673f6
nn200057 <nathael.nogues@hotmail.fr>
2016-11-16 19:06:18 +0100

    Test corrected #Fail

--- crossroad/src/test/java/fr/unice/polytech/al/trafficlight/crossroad/TestEmergencyCall.java
+++ crossroad/src/test/java/fr/unice/polytech/al/trafficlight/crossroad/TestEmergencyCall.java
@@ -71,7 +71,7 @@ public class TestEmergencyCall {
         assertNull(module.getActiveScenario());
 
         // Launch scenario
-        module.changeScenario(new Gson().toJson(scenario));
+        module.changeScenario(scenario);
         LOG.debug("TEST>START");
 
         sleep(1);
@@ -93,7 +93,7 @@ public class TestEmergencyCall {
         checkTrafficLightRed(module.getTrafficLights(), group1, group2);
 
         // Calling to emergency while red
-        module.callEmergency(new Gson().toJson(emergencyCall));
+        module.callEmergency(emergencyCall);
 
         // When called while trafficLights red, emergency call should
         // immediately call the red step of emergency state.
@@ -121,7 +121,7 @@ public class TestEmergencyCall {
         checkTrafficLightStep(module.getTrafficLights(), group2);
 
         // Stopping
-        module.stopTrafficLight(); // should stop after (2nd) group1 red step
+        module.stopRunning(); // should stop after (2nd) group1 red step
         sleep(5);
         LOG.debug("TEST>Wait 5s (1s after Group2 Green Step)");
         assertFalse(module.runnable.isRunning());
@@ -147,7 +147,7 @@ public class TestEmergencyCall {
         assertNull(module.getActiveScenario());
 
         // Launch scenario
-        module.changeScenario(new Gson().toJson(scenario));
+        module.changeScenario(scenario);
         LOG.debug("TEST>START");
 
         sleep(1);
@@ -164,7 +164,7 @@ public class TestEmergencyCall {
         checkTrafficLightStep(module.getTrafficLights(), group1);
 
         // Calling to emergency while green
-        module.callEmergency(new Gson().toJson(emergencyCall));
+        module.callEmergency(emergencyCall);
 
         // When called while trafficLights red, emergency call should
         // immediately call the red step of emergency state.
@@ -192,7 +192,7 @@ public class TestEmergencyCall {
         checkTrafficLightStep(module.getTrafficLights(), group2);
 
         // Stopping
-        module.stopTrafficLight(); // should stop after (2nd) group1 red step
+        module.stopRunning(); // should stop after (2nd) group1 red step
         sleep(5);
         LOG.debug("TEST>Wait 5s (1s after Group2 Green Step)");
         assertFalse(module.runnable.isRunning());
