9a0a2795cba208c3f128023f1febb664ce4fcc8f
Lydwen <tomlau94@aol.com>
2017-01-20 10:50:10 +0100

    Add tests for the graph

---
+++ central/src/test/java/fr/unice/polytech/al/trafficlight/central/data/graph/EdgeTest.java
@@ -0,0 +1,66 @@
+package fr.unice.polytech.al.trafficlight.central.data.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 12/01/17.
+ */
+public class EdgeTest {
+    @Test
+    public void testGetters(){
+        Edge<String> edge = new Edge<>("beginNode","endNode", 42);
+
+        assertEquals("beginNode", edge.getBegin());
+        assertEquals("endNode", edge.getEnd());
+        assertEquals(42, edge.getWeight());
+    }
+
+    @Test
+    public void testSetters(){
+        Edge<String> edge = new Edge<>("no one care", "no one care", -12);
+
+        edge.setBegin("new begin");
+        assertEquals("new begin", edge.getBegin());
+
+        edge.setEnd("new end");
+        assertEquals("new end", edge.getEnd());
+
+        edge.setWeight(23);
+        assertEquals(23, edge.getWeight());
+    }
+
+    @Test
+    public void testEquals(){
+        Edge<String> edge = new Edge<>("beginNode","endNode", 42);
+        Edge<String> edgeClone = new Edge<>("beginNode","endNode", 42);
+
+        assertEquals("the edge must be equals to himself",
+                edge, edge);
+        assertEquals("two edges are equals if they have the same begin node, end node, and weight",
+                edge, edgeClone);
+    }
+
+    @Test
+    public void testNotEquals(){
+        Edge<String> edge = new Edge<>("beginNode","endNode", 42);
+        Edge<String> edgeClone = new Edge<>("beginNode","endNode", 42);
+
+        edge.setBegin("new node");
+        assertNotEquals(edgeClone, edge);
+
+        edge.setBegin("beginNode");
+        assertEquals(edgeClone, edge);
+
+        edge.setEnd("new node");
+        assertNotEquals(edgeClone, edge);
+
+        edge.setEnd("endNode");
+        assertEquals(edgeClone, edge);
+
+        edge.setWeight(12);
+        assertNotEquals(edgeClone, edge);
+    }
+
+}
