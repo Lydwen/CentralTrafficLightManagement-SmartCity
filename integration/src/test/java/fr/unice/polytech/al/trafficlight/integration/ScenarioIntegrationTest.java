2dc71e27288dc84bc172c32837fff6464d817ff7
Kévin Buisson <buisson.kevin2@gmail.com>
2017-01-26 22:47:43 +0100

    [DEV] Scenario integration test

--- integration/src/test/java/fr/unice/polytech/al/trafficlight/integration/ScenarioIntegrationTest.java
+++ integration/src/test/java/fr/unice/polytech/al/trafficlight/integration/ScenarioIntegrationTest.java
@@ -28,11 +28,14 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Emergency integration test.
+ * Scenario integration test.
+ * This test asserts that when you submit a scenario from the web-app, the scenario is well deployed on the central, and then on the crossroad.
  */
 @Ignore
 public class ScenarioIntegrationTest {
-    public static final String WEB_APP_URL = "https://webapp-traffic-light.herokuapp.com/";
+    private static final String WEBAPP_URL = "https://webapp-traffic-light.herokuapp.com/";
+    private static final String CENTRAL_URL = "https://central-traffic-light.herokuapp.com/";
+    private static final String CASINO_CROSSROAD = "carrefour_du_casino";
     private static Properties crossroadsUrls;
 
     @BeforeClass
@@ -43,16 +46,17 @@ public class ScenarioIntegrationTest {
 
     @Test
     public void testSetScenario() {
-        final String crossroad = "carrefour_du_casino";
         final String scenarioFormData = "transitionTime=5&" +
                 "nbGroups=4&" +
                 "0=3&t0=gauche&t0=droite&" +
-                "1=5&t1=bas_gauche&t1=haut_droite";
+                "1=4&t1=haut&t1=bas&" +
+                "2=5&t2=bas_gauche&t2=haut_droite&" +
+                "3=6&t3=bas_droite&t3=haut_gauche";
         RestTemplate restTemplate = new RestTemplate();
 
         // Set the scenario using the webapp
         restTemplate.postForEntity(
-                WEB_APP_URL + crossroad,
+                WEBAPP_URL + CASINO_CROSSROAD,
                 buildScenarioRequest(scenarioFormData),
                 String.class);
 
@@ -63,15 +67,30 @@ public class ScenarioIntegrationTest {
             e.printStackTrace();
         }
 
+        // Get the scenario from the central
+        {
+            System.out.println(CENTRAL_URL + "crossroad/" + CASINO_CROSSROAD + "/scenario");
+            Scenario scenario = restTemplate.getForEntity(
+                    CENTRAL_URL + "crossroad/" + CASINO_CROSSROAD + "/scenario", Scenario.class).getBody();
+            assertCasinoScenario(scenario);
+        }
+
         // Get the scenario from the crossroad
-        Scenario scenario = restTemplate.getForEntity(crossroadsUrls.getProperty(crossroad) + "/scenario", Scenario.class).getBody();
+        {
+            Scenario scenario = restTemplate.getForEntity(
+                    crossroadsUrls.getProperty(CASINO_CROSSROAD) + "/crossroad/scenario", Scenario.class).getBody();
+            assertCasinoScenario(scenario);
+        }
+    }
+
+    private static void assertCasinoScenario(Scenario scenario) {
         assertNotNull(scenario);
 
-        assertEquals(crossroad, scenario.getId());
+        assertEquals(CASINO_CROSSROAD, scenario.getId());
         assertEquals(5, scenario.getTransitionTime());
 
         List<RuleGroup> ruleGroups = scenario.getRuleGroupList();
-        assertEquals(2, ruleGroups.size());
+        assertEquals(4, ruleGroups.size());
         {
             RuleGroup ruleGroup = ruleGroups.get(0);
             assertEquals("group0", ruleGroup.getId());
@@ -83,12 +102,31 @@ public class ScenarioIntegrationTest {
         {
             RuleGroup ruleGroup = ruleGroups.get(1);
             assertEquals("group1", ruleGroup.getId());
+            assertEquals(4, ruleGroup.getMinimumGreenTime());
+            assertEquals(4, ruleGroup.getNormalGreenTime());
+            assertEquals(2, ruleGroup.getTrafficLights().size());
+            assertContainsAll(ruleGroup.getTrafficLights(), "haut", "bas");
+        }
+        {
+            RuleGroup ruleGroup = ruleGroups.get(2);
+            assertEquals("group2", ruleGroup.getId());
             assertEquals(5, ruleGroup.getMinimumGreenTime());
             assertEquals(5, ruleGroup.getNormalGreenTime());
             assertEquals(2, ruleGroup.getTrafficLights().size());
             assertContainsAll(ruleGroup.getTrafficLights(), "bas_gauche", "haut_droite");
         }
+        {
+            RuleGroup ruleGroup = ruleGroups.get(3);
+            assertEquals("group3", ruleGroup.getId());
+            assertEquals(6, ruleGroup.getMinimumGreenTime());
+            assertEquals(6, ruleGroup.getNormalGreenTime());
+            assertEquals(2, ruleGroup.getTrafficLights().size());
+            assertContainsAll(ruleGroup.getTrafficLights(), "bas_droite", "haut_gauche");
+        }
     }
+
+    // ====================================================
+    // ====================================================
 
     private static void assertContainsAll(Set<TrafficLightId> trafficLights, String... ids) {
         assertTrue(trafficLights.stream()
